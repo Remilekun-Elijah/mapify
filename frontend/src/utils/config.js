ae13d0b451fb548262053bb3e2a6ca1e23311893
= <=>
2022-09-01 15:21:37 +0100

    updated backend url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,18 +1,14 @@
 const environment = {}; 
 environment.development = {
- backendUrl: "http://localhost:9000",
- // backendUrl: "http://138.68.130.181:9000/",
-
-//  cloudinary_cloudName: process.env.REACT_APP_CLOUDINARY_CLOUDNAME,
-//  cloudinary_preset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
-//  cloudinary_folder: process.env.REACT_APP_CLOUDINARY_UPLOAD_FOLDER
+ // backendUrl: "http://localhost:9000",
+ "backendUrl": "https://api-mapify.onrender.com",
 cloudinary_cloudName:"remilekunelijah",
  cloudinary_preset: "ixp9w0hy",
  cloudinary_folder: "Mapify"
 }
 
 environment.production = {
- backendUrl: "http://138.68.130.181:5000/",
+ backendUrl: "https://api-mapify.onrender.com",
  
  cloudinary_cloudName:"remilekunelijah",
  cloudinary_preset: "ixp9w0hy",
