4fc03bfa8568581f69eb3698ce1aa10a67c058f5
RemilekunElijah <remilekunelijah21997@gmail.com>
2022-12-17 18:33:07 +0100

    updated config

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,13 @@
 const environment = {}; 
 environment.development = {
- backendUrl: "http://localhost:9000",
+ backendUrl: "http://localhist:9000",
+cloudinary_cloudName:"remilekunelijah",
+ cloudinary_preset: "ixp9w0hy",
+ cloudinary_folder: "Mapify"
+}
+
+environment.staging = {
+ backendUrl: "https://api-staging-mapify.onrender.com",
 cloudinary_cloudName:"remilekunelijah",
  cloudinary_preset: "ixp9w0hy",
  cloudinary_folder: "Mapify"
